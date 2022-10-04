3b831a787e020498b6d3cba35f9562a356c28933
MiguelDotL <miguel.d.lozano@gmail.com>
2022-10-04 16:31:47 -0400

    add redux boilerplate and structure out components

---
+++ src/components/PostList.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class PostList extends Component {
+    render() {
+        return <div className="ui list">Post List 😄</div>;
+    }
+}
+
+export default PostList;
